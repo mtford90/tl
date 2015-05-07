9926979c45764c8ab7ca3d12ad2aa4b3287f5d16
Michael Ford <mtford@gmail.com>
2015-05-07 14:26:48 +0100

    attempt to fix staticfiles

--- server/toptal/settings.py
+++ server/toptal/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'server/staticfiles'
+STATIC_ROOT = '/app/server/staticfiles'
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (
